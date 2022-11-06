f0c6fc1c14b8335da978d02d6ba0d15d608e96d6
bhjani <90314337+bhjani@users.noreply.github.com>
2022-11-06 17:39:53 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,46 +1,20 @@
-// const calculate = document.getElementById("btn1");
-// const background = document.getElementById("btn2");
-// const output = document.getElementById("output");
-
-// if (!!window.SharedWorker) {
-//     const worker =  new SharedWorker("worker.js");
-    
-//     background.onclick = function(e) {
-//         document.body.style.backgroundColor = document.body.style.backgroundColor !== "red" ? "red" : "yellow";
-//     }
-
-//     calculate.onclick = function() {
-//         worker.port.postMessage({number: 100000000});
-//     }
-
-//     worker.port.onmessage = function (e) {
-//         // output.value = e.data.number;
-//         console.log("output -------->", e.data);
-//     }
-// }
-
-
-const first = document.querySelector("#number1");
-const second = document.querySelector("#number2");
-
-const result1 = document.querySelector(".result1");
+const calculate = document.getElementById("btn1");
+const background = document.getElementById("btn2");
+const output = document.getElementById("output");
 
 if (!!window.SharedWorker) {
-  const myWorker = new SharedWorker("worker.js");
+    const worker =  new SharedWorker("worker.js");
+    
+    background.onclick = function(e) {
+        document.body.style.backgroundColor = document.body.style.backgroundColor !== "red" ? "red" : "yellow";
+    }
 
-  first.onchange = function () {
-    myWorker.port.postMessage([first.value, second.value]);
-    console.log("Message posted to worker");
-  };
+    calculate.onclick = function() {
+        worker.port.postMessage({number: 100000000});
+    }
 
-  second.onchange = function () {
-    myWorker.port.postMessage([first.value, second.value]);
-    console.log("Message posted to worker");
-  };
-
-  myWorker.port.onmessage = function (e) {
-    result1.textContent = e.data;
-    console.log("Message received from worker");
-    console.log(e.lastEventId);
-  };
+    worker.port.onmessage = function (e) {
+        // output.value = e.data.number;
+        console.log("output -------->", e.data);
+    }
 }
