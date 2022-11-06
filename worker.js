265f95df45ed6653d74f4ec7fd582f7139cd5d76
bhjani <90314337+bhjani@users.noreply.github.com>
2022-11-06 18:02:10 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,10 @@ onconnect = function(e) {
     const port = e.ports[0];
 
     port.onmessage = (e) => {
-        for (let i = 0; i < e.data; i++) {};
-        port.postMessage({numer: e.data});
+        console.log("worker start.........", e);
+        console.log("worker start.........", e.data[0]);
+        for (let i = 0; i < e.data[0]; i++) {};
+        console.log("worker end.........", e.data[0]);
+        port.postMessage({numer: e.data[0]});
     }
 }
