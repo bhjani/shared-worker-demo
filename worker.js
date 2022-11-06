e98b2f4f17c9815851c64ea8df759f7997f69239
bhjani <90314337+bhjani@users.noreply.github.com>
2022-11-06 17:40:15 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,19 +1,9 @@
-// onconnect = function(e) {
-//     console.log("connected.........");
-//     const port = e.ports[0];
+onconnect = function(e) {
+    console.log("connected.........");
+    const port = e.ports[0];
 
-//     port.onmessage = (e) => {
-//         for (let i = 0; i < e.data.numer; i++) {};
-//         port.postMessage({numer: e.data.numer});
-//     }
-// }
-
-onconnect = function (event) {
-    const port = event.ports[0];
-  
-    port.onmessage = function (e) {
-    //   const workerResult = `Result: ${e.data[0] * e.data[1]}`;
-      port.postMessage("worker is working");
-    };
-    port.start();
-  };
+    port.onmessage = (e) => {
+        for (let i = 0; i < e.data.numer; i++) {};
+        port.postMessage({numer: e.data.numer});
+    }
+}
